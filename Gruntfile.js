73d44430ee5c6a515c938976392832c6b292a414
gsellator <adrien@gsell.me>
2016-02-09 14:19:50 +0100

    style fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,99 +1,112 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-    grunt.initConfig({
+  grunt.initConfig({
 
-        pkg: grunt.file.readJSON('package.json'),
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
 
-        clean: ["pkg/"],
+    clean: ["pkg/"],
+    
+    sass: {
+      dev: {
+        files: [{
+          expand: true,
+          cwd: 'public/scss/',
+          src: ['*.scss'],
+          dest: 'public/css',
+          ext: '.css'
+        }]
+      },
+    },
 
-        env : {
-            options : {
-            },
-            dev: {
-                NODE_ENV : 'DEVELOPMENT'
-            },
-            prod : {
-                NODE_ENV : 'PRODUCTION'   
-            }
+    env : {
+      options : {
+      },
+      dev: {
+        NODE_ENV : 'DEVELOPMENT'
+      },
+      prod : {
+        NODE_ENV : 'PRODUCTION'
+      }
+    },
+
+    bump: {
+      options: {
+        commit: false,
+        createTag: false,
+        push: false
+      }
+    },
+
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'public/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'pkg/public'
+        }]
+      }
+    },
+
+    copy: {
+      package: {
+        src: 'package.json',
+        dest: 'pkg/',
+      },
+      server1: {
+        src: 'app.js',
+        dest: 'pkg/',
+      },
+      server2: {
+        src: 'views/**/*',
+        dest: 'pkg/',
+      },
+      server3: {
+        src: 'public/css/*',
+        dest: 'pkg/',
+      },
+      //            server4: {
+      //                src: 'public/docs/*',
+      //                dest: 'pkg/',
+      //            },
+      server5: {
+        src: 'public/js/*',
+        dest: 'pkg/',
+      },
+      server6: {
+        src: 'public/favicon.ico',
+        dest: 'pkg/',
+      },
+      server7: {
+        src: 'public/favicon.png',
+        dest: 'pkg/',
+      }
+    },
+
+    'sftp-deploy': {
+      prod: {
+        auth: {
+          host: '91.121.177.78',
+          port: 22,
+          authKey: 'prod'
         },
+        src: 'pkg',
+        dest: '/home/GsellMe-prod/GsellMe',
+        exclusions: ['pkg/package.json', 'pkg/server/*'],
+        server_sep: '/'
+      }
+    },
+  });
 
-        bump: {
-            options: {
-                commit: false,
-                createTag: false,
-                push: false
-            }
-        },
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-env');
+  grunt.loadNpmTasks('grunt-bump');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-sftp-deploy');
 
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: 'public/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'pkg/public'
-                }]
-            }
-        },
-
-        copy: {
-            package: {
-                src: 'package.json',
-                dest: 'pkg/',
-            },
-            server1: {
-                src: 'app.js',
-                dest: 'pkg/',
-            },
-            server2: {
-                src: 'views/**/*',
-                dest: 'pkg/',
-            },
-            server3: {
-                src: 'public/css/*',
-                dest: 'pkg/',
-            },
-            //            server4: {
-            //                src: 'public/docs/*',
-            //                dest: 'pkg/',
-            //            },
-            server5: {
-                src: 'public/js/*',
-                dest: 'pkg/',
-            },
-            server6: {
-                src: 'public/favicon.ico',
-                dest: 'pkg/',
-            },
-            server7: {
-                src: 'public/favicon.png',
-                dest: 'pkg/',
-            }
-        },
-
-        'sftp-deploy': {
-            prod: {
-                auth: {
-                    host: '91.121.177.78',
-                    port: 22,
-                    authKey: 'prod'
-                },
-                src: 'pkg',
-                dest: '/home/GsellMe-prod/GsellMe',
-                exclusions: ['pkg/package.json', 'pkg/server/*'],
-                server_sep: '/'
-            }
-        },
-    });
-
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-env');
-    grunt.loadNpmTasks('grunt-bump');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-sftp-deploy');
-
-    grunt.registerTask('prod', ['env:prod', 'clean', 'bump', 'imagemin', 'copy', 'sftp-deploy:prod']);
+  grunt.registerTask('prod', ['env:prod', 'clean', 'bump', 'imagemin', 'copy', 'sftp-deploy:prod']);
 };
