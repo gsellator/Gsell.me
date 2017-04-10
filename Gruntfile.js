a82a1ceb3ef85e76998d7c60010e08da1b5c8889
Adrien <adrien@gsell.me>
2017-04-10 12:05:38 +0200

    Test

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,6 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-
-    pkg: grunt.file.readJSON('package.json'),
-    banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
-
-    clean: ["pkg/"],
-    
     sass: {
       dev: {
         files: [{
@@ -19,94 +13,8 @@ module.exports = function(grunt) {
         }]
       },
     },
-
-    env : {
-      options : {
-      },
-      dev: {
-        NODE_ENV : 'DEVELOPMENT'
-      },
-      prod : {
-        NODE_ENV : 'PRODUCTION'
-      }
-    },
-
-    bump: {
-      options: {
-        commit: false,
-        createTag: false,
-        push: false
-      }
-    },
-
-    imagemin: {
-      dynamic: {
-        files: [{
-          expand: true,
-          cwd: 'public/',
-          src: ['**/*.{png,jpg,gif}'],
-          dest: 'pkg/public'
-        }]
-      }
-    },
-
-    copy: {
-      package: {
-        src: 'package.json',
-        dest: 'pkg/',
-      },
-      server1: {
-        src: 'app.js',
-        dest: 'pkg/',
-      },
-      server2: {
-        src: 'views/**/*',
-        dest: 'pkg/',
-      },
-      server3: {
-        src: 'public/css/*',
-        dest: 'pkg/',
-      },
-      //            server4: {
-      //                src: 'public/docs/*',
-      //                dest: 'pkg/',
-      //            },
-      server5: {
-        src: 'public/js/*',
-        dest: 'pkg/',
-      },
-      server6: {
-        src: 'public/favicon.ico',
-        dest: 'pkg/',
-      },
-      server7: {
-        src: 'public/favicon.png',
-        dest: 'pkg/',
-      }
-    },
-
-    'sftp-deploy': {
-      prod: {
-        auth: {
-          host: '91.121.177.78',
-          port: 22,
-          authKey: 'prod'
-        },
-        src: 'pkg',
-        dest: '/home/GsellMe-prod/GsellMe',
-        exclusions: ['pkg/package.json', 'pkg/server/*'],
-        server_sep: '/'
-      }
-    },
   });
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-env');
-  grunt.loadNpmTasks('grunt-bump');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-sftp-deploy');
-
-  grunt.registerTask('prod', ['env:prod', 'clean', 'bump', 'imagemin', 'copy', 'sftp-deploy:prod']);
+  grunt.registerTask('default', ['sass']);
 };
---
+++ server.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var server = express();
+
+server.set('view engine', 'ejs');
+server.use(express.static(__dirname + '/public'));
+server.use(bodyParser.json());
+
+//Routes
+server.get('/', function(req, res) { res.render('index.ejs'); });
+//server.get('/projects', function(req, res) { res.render('projects.ejs'); });
+//server.get('/docs', function(req, res) { res.render('docs.ejs'); });
+
+server.use(function(req, res, next){
+    res.setHeader('Content-Type', 'text/plain');
+    res.send(404, 'Page introuvable !');
+});
+
+server.set("port", process.env.PORT || 3000);
+
+var activeServer = server.listen(server.get("port"), () => {
+  console.log(`Express ${server.get("env")} server listening on ${server.get("port")}`);
+});
